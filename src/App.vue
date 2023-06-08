e297fd1e703de2d92eba15fac0af0cc7b71ed153
ealbiol <eduard.albiol@gmail.com>
2023-06-08 14:29:49 +0200

    Creating App.vue and rendering a list of learning resources.

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<!--Root Component-->
+<template>
+  <ul>
+    <!--Passing props to RescourceItem : :resource="resource"-->
+    <ResourceItem
+      :resource="resource"
+      v-for="resource in storedResources"
+      :key="resource.id"
+    />
+  </ul>
+</template>
+<script>
+import ResourceItem from './components/resources/ResourceItem.vue';
+
+export default {
+  components: {
+    ResourceItem,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'http://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'http://google.com',
+        },
+        {
+          id: 'chatgpt',
+          title: 'Chat GPT',
+          description: 'Learn to ask...',
+          link: 'https://chat.openai.com/',
+        },
+      ],
+    };
+  },
+};
+</script>
+<!--Global Styles-->
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/resources/ResourceItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ resource.title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ resource.description }}</p>
+    <nav>
+      <a :href="resource.link" target="_blank">View Resource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  //Receiving props from App.vue
+  props: ['resource'],
+};
+</script>
+<style></style>
